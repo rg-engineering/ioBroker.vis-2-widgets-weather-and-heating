c387cf1cead7c29dc67d594820fc77129f6fec96
René <info@rg-engineering.eu>
2024-04-19 19:26:01 +0200

    many improvements

--- src-widgets/src/GeneralEChartWidget.jsx
+++ src-widgets/src/GeneralEChartWidget.jsx
@@ -22,11 +22,13 @@ const styles = () => ({
 });
 
 
-//todo Anpassunge aus Wetter widget übernehmen
+
 //todo readme anpassen -> Beschreibung Widget und JSON Input
-//todo JSON Input als Datenquelle
+//todo JSON Input als Datenquelle -> geht noch nicht
 //todo mehrere Inputs unterstützen
 //todo Übersetzungen
+//todo Y Achse position pro serie
+
 
 class GeneralEChartWidget extends (Generic) {
 
@@ -72,26 +74,81 @@ class GeneralEChartWidget extends (Generic) {
 
                             default: 'headline',
                         },
+                        {
+                            name: 'dataCount',
+                            type: 'number',
+                            label: 'widgets_echart_label_datacount',
+                            default: 1,
+                        },
                         
                     ],
                 },
                 {
                     name: 'data', // group name
+                    label: 'data',
+                    indexFrom: 1,
+                    indexTo: 'dataCount',
                     fields: [
                         {
                             name: 'oid_data',    // name in data structure
                             label: 'widgets_echart_label_oiddata', // translated field label
                             type: 'id',
-
                             default: "",
-
+                        },
+                        {
+                            name: 'data_seriestype',    // name in data structure
+                            label: 'widgets_echart_data_seriestype', // translated field label
+                            type: 'select',
+                            options: [
+                                {
+                                    value: 'bar',
+                                    label: 'widgets_echart_seriestype_bar'
+                                },
+                                {
+                                    value: 'line',
+                                    label: 'widgets_echart_seriestype_line'
+                                }],
+                            default: "bar",
+                        },
+                        {
+                            name: 'data_unit',    // name in data structure
+                            label: 'widgets_echart_data_unit', // translated field label
+                            type: 'text',
+                            default: "",
                         },
                         {
                             name: 'data_color',    // name in data structure
-                            label: 'widgets_echart_label_data_color', // translated field label
+                            label: 'widgets_echart_data_color', // translated field label
                             type: 'color',
 
                             default: "yellow",
+
+                        },
+                        {
+                            name: 'data_yaxispos',    // name in data structure
+                            label: 'widgets_echart_data_yaxispos', // translated field label
+                            type: 'select',
+                            options: [
+                                {
+                                    value: 'none',
+                                    label: 'widgets_echart_data_yaxispos_none'
+                                },
+                                {
+                                    value: 'left',
+                                    label: 'widgets_echart_data_yaxispos_left'
+                                },
+                                {
+                                    value: 'right',
+                                    label: 'widgets_echart_data_yaxispos_right'
+                                }],
+                            default: "left",
+                        },
+                        {
+                            name: 'data_calcdiff',    // name in data structure
+                            label: 'widgets_echart_data_calcdiff', // translated field label
+                            type: 'checkbox',
+
+                            default: "false",
 
                         },
                     ]
@@ -174,10 +231,10 @@ class GeneralEChartWidget extends (Generic) {
         //todo serien einstellbar
         //todo XAchse einstellbar
 
+        console.log("getOption 1");
+        
 
-        const data = this.state.values[`${this.state.rxData['oid_data']}.val`];
-
-        console.log("data " + JSON.stringify(data));
+       
 
         let dataMin = 0;
         let dataMax = 100;
@@ -190,35 +247,171 @@ class GeneralEChartWidget extends (Generic) {
 
         let cnt = 0;
 
-        if (data && data.length>1) {
+        
 
-            legend.push("data");
+        console.log("getOption 2");
+
+/*
+gechart values{ "sbfspot.0.2000562095.history.years.val": "[{\"year\":\"2008\",\"value\":7000},{\"year\":\"2009\",\"value\":2309000},{\"year\":\"2010\",\"value\":4445000},{\"year\":\"2011\",\"value\":7019000},{\"year\":\"2012\",\"value\":9371000},{\"year\":\"2013\",\"value\":11393000},{\"year\":\"2014\",\"value\":13666000},{\"year\":\"2015\",\"value\":16034000},{\"year\":\"2016\",\"value\":17826790}]", "sbfspot.0.2000562095.history.years.ack": true, "sbfspot.0.2000562095.history.years.ts": 1660977902396, "sbfspot.0.2000562095.history.years.q": 0, "sbfspot.0.2000562095.history.years.from": "system.adapter.sbfspot.0", "sbfspot.0.2000562095.history.years.user": "system.user.admin", "sbfspot.0.2000562095.history.years.lc": 1660725002668 } src_GeneralEChartWidget_jsx.8424614c.chunk.js: 1: 2855
+gechart rxData { "bindings": [], "oid_data": "sbfspot.0.2000562095.history.years", "g_common": true, "headline": "head", "xaxis_axisLabel_formatstring": "undefined undefined:undefined", "g_X_axis": true, "noCard": false, "dataCount": "1", "oid_data1": "sbfspot.0.2000562095.history.years", "data_color1": "yellow", "g_data-1": true }
+*/
+
+        for (let d = 1; d <= this.state.rxData["dataCount"]; d++) {
+
+            let OID_name = 'oid_data' + d;
+            const OID = this.state.rxData[OID_name];
+            const OID_val = OID + ".val";
+            const data_org1 = this.state.values[OID_val];
+
+            console.log("data" + d + " : " + " " + OID_name + OID + " " + OID_val + " " + JSON.stringify(data_org1) + " " + "  data_calcdiff${d}  " + this.state.rxData["data_calcdiff${d}"]);
+
+            let data = [];
+
+            /*
+            sbfspot.0.2000562095.history.years.val "[
+                {\"year\":\"2008\",\"value\":7000},
+                {\"year\":\"2009\",\"value\":2309000},
+                {\"year\":\"2010\",\"value\":4445000},
+                {\"year\":\"2011\",\"value\":7019000},
+                {\"year\":\"2012\",\"value\":9371000},
+                {\"year\":\"2013\",\"value\":11393000},
+                {\"year\":\"2014\",\"value\":13666000},
+                {\"year\":\"2015\",\"value\":16034000},
+                {\"year\":\"2016\",\"value\":17826790}]"
+
+            vs
+
+            options: {
+            
+            "series":[{"name":"sun","type":"bar",
+            "data":[
+                ["2024-04-13T00:00:00.000Z",42],
+                ["2024-04-13T03:00:00.000Z",34],
+                ["2024-04-13T06:00:00.000Z",63],
+                ["2024-04-13T09:00:00.000Z",45],
+                ["2024-04-13T12:00:00.000Z",51],
+                ["2024-04-13T15:00:00.000Z",50],
+                ["2024-04-13T18:00:00.000Z",50],
+                ["2024-04-13T21:00:00.000Z",50]]
+
+                */
+
+
+
+
+
+
+
+
+            if (data_org1 && data_org1.length > 1) {
+
+                const data_org = JSON.parse(data_org1);
+
+                let lastval4diff = 0;
+
+                for (var i = 0; i < data_org.length; i++) {
+
+                    var oVals = data_org[i];
+
+                    //todo keys einstellbar
+                    var year = parseInt(oVals["year"], 10);
+                    var value = parseInt(oVals["value"], 10);
+
+                    var oDate = new Date(year, 5, 30, 12, 0, 0, 0);
+
+
+                    OID_name = 'data_calcdiff' + d;
+
+                    if (this.state.rxData[OID_name]) {
+                        if (i == 0) {
+                            lastval4diff = value;
+                        }
+                        else {
+                            let newvalue = value - lastval4diff;
+                            lastval4diff = value;
+                            value = newvalue;
+                        }
+                    }
+
+
+
+                    if (value < dataMin) { dataMin = value; }
+                    if (value > dataMax) { dataMax = value; }
+
+                    console.log("push # " + year + " " + value);
+                    data.push(
+                        [
+                            oDate,
+                            value
+                        ]
+                    )
+                }
+
+                //todo min max berechnen -> testen
+                //todo type einstellbar -> testen
+                //todo x Achse labe format einstellbar
+                //todo y achse unit einstellbar -> testen
+                //todo x achse unit einstellbar
+                //todo x achse type (tome or category) einstellbar
+                //todo option: diff aus aufeinander folgenden werten berechnen -> testen
+                //todo autoumrechnung w -> kW usw. ???
+                //todo keys einstellbar
+                //todo einstellbares format (reine liste, oder object liste)
+
+
+
+            }
+
+            if (data && data.length > 0) {
+
+                legend.push("data" + d);
+
+                var keys = Object.keys(data[0]);
+                console.log("keys " + keys);
+
+                const type_id = "data_seriestype" + d;
+                let type = this.state.rxData[type_id];
+                const color_id = "data_color$" + d;
+                let color = this.state.rxData[color_id];
+
+                series.push({
+                    name: "data",
+                    type: type,
+                    data: data,
+                    color: color,
+                    yAxisIndex: cnt,
+                    tooltip: {
+                        valueFormatter: function (value) {
+                            return value + ' ';
+                        }
+                    },
+                });
+                cnt++;
+            }
+
+
+
+            const unit_id = "data_unit" + d;
+            let unit = this.state.rxData[unit_id];
+            const yaxispos_id = "data_yaxispos" + d;
+            let yaxispos = this.state.rxData[yaxispos_id];
+
+            
 
             yaxis.push({
-                position: "left",
+                position: yaxispos,
+                show: yaxispos=="none" ? false : true,
                 type: "value",
                 min: dataMin,
                 max: dataMax,
                 axisLabel: {
-                    formatter: '{value} '
+                    formatter: function (value) {
+                        return value + ' ' + unit;
+                    }
+
                 }
             });
-            series.push({
-                name: "data",
-                type: 'bar',
-                data: data,
-                color: this.state.rxData['rain_color'] || "yellow",
-                yAxisIndex: cnt,
-                tooltip: {
-                    valueFormatter: function (value) {
-                        return value + ' ';
-                    }
-                },
-            });
-            cnt++;
         }
-
-
 
         let useSecondDiagram = false;
 
@@ -234,7 +427,9 @@ class GeneralEChartWidget extends (Generic) {
                 min: 0,
                 max: 100,
                 axisLabel: {
-                    formatter: '{value} %'
+                    formatter: function (value) {
+                        return value ;
+                    }
                 }
             });
             series.push({
@@ -293,7 +488,7 @@ class GeneralEChartWidget extends (Generic) {
 
                     rotate: 45,
                     //todo format einstellbar
-                    formatter: '{ee} {hh}:{mm}',
+                    formatter: '{yyyy}',
                     //formatter: axisLabel_formatstring,
                 }
 
@@ -311,11 +506,26 @@ class GeneralEChartWidget extends (Generic) {
 
 
 
+    /*
 
+    options: {"backgroundColor":"transparent","title":{"text":"head"},"grid":{"show":true,"top":30,"bottom":60},"tooltip":{"trigger":"axis"},"legend":{"data":["data1"],"orient":"horizontal","right":10},"xAxis":{"type":"time","show":true,"axisLabel":{"rotate":45,"formatter":"{yy}"}},"yAxis":[{"position":"left","type":"value","min":0,"max":17826790,"axisLabel":{"formatter":"{value} undefined"}}],"series":[{"name":"data","type":"bar","data":[["2008-06-30T10:00:00.000Z",7000],["2009-06-30T10:00:00.000Z",2309000],["2010-06-30T10:00:00.000Z",4445000],["2011-06-30T10:00:00.000Z",7019000],["2012-06-30T10:00:00.000Z",9371000],["2013-06-30T10:00:00.000Z",11393000],["2014-06-30T10:00:00.000Z",13666000],["2015-06-30T10:00:00.000Z",16034000],["2016-06-30T10:00:00.000Z",17826790]],"color":"yellow","yAxisIndex":0,"tooltip":{}}]}
+
+
+
+
+    */
    
 
     renderWidgetBody(props) {
         super.renderWidgetBody(props);
+
+
+        console.log("gechart values" + JSON.stringify(this.state.values));
+        console.log("gechart rxData " + JSON.stringify(this.state.rxData));
+
+        
+
+
 
 
         let size;

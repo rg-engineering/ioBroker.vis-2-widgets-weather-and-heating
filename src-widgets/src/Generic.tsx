25b24bd50b149a4f4e2bda657f74de0112f04216
René <info@rg-engineering.eu>
2025-10-26 12:36:29 +0100

    again linter based change

--- src-widgets/src/Generic.tsx
+++ src-widgets/src/Generic.tsx
@@ -1,11 +1,11 @@
 
 import type { VisRxWidgetState } from '@iobroker/types-vis-2';
-import type VisRxWidget from '@iobroker/types-vis-2/visRxWidget';
+//import type VisRxWidget from '@iobroker/types-vis-2/visRxWidget';
 
 export default class Generic<
     RxData extends Record<string, any>,
     State extends Partial<VisRxWidgetState> = VisRxWidgetState,
-> extends (window.visRxWidget as typeof VisRxWidget)<RxData, State> {
+> extends (window.visRxWidget)<RxData, State> {
     getPropertyValue = (stateName: string): any => this.state.values[`${(this.state.rxData as any)[stateName]}.val`];
 
     static getI18nPrefix(): string {

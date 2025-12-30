e2d50589987a126fcce468ac2623c9726f2057b8
René <info@rg-engineering.eu>
2025-12-30 15:40:19 +0100

    right type info added

--- src-widgets/src/Generic.tsx
+++ src-widgets/src/Generic.tsx
@@ -1,11 +1,20 @@
 
-import type { VisRxWidgetState } from '@iobroker/types-vis-2';
-//import type VisRxWidget from '@iobroker/types-vis-2/visRxWidget';
+import type {
+    //RxRenderWidgetProps,
+    //RxWidgetInfo,
+    //VisRxWidgetProps,
+    //WidgetData,
+    VisRxWidgetState,
+    //RxWidgetInfoAttributesField
+} from '@iobroker/types-vis-2';
+
+import type VisRxWidget from '@iobroker/types-vis-2/visRxWidget';
 
 export default class Generic<
     RxData extends Record<string, any>,
     State extends Partial<VisRxWidgetState> = VisRxWidgetState,
-> extends (window.visRxWidget)<RxData, State> {
+    > extends (window.visRxWidget as typeof VisRxWidget)<RxData, State> {
+
     getPropertyValue = (stateName: string): any => this.state.values[`${(this.state.rxData as any)[stateName]}.val`];
 
     static getI18nPrefix(): string {
